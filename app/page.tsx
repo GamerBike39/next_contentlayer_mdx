2b917465a99b321c2e53a34f21875a1e2123dbe1
GamerBike39 <champidub@gmail.com>
2023-07-22 15:05:49 +0200

    svg text

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 "use client";
+import CircleText from "@/components/ui/specialWords/CircleText";
+import UnderlineVagueSvg from "@/components/ui/specialWords/Underline_Vague";
 import { useViewportSize } from "@/hooks/use-viewport-size/use-viewport-size";
 
 export default function Home() {
   const { height, width } = useViewportSize();
 
   return (
-    <div className="my-10 w-full bg-zinc-900">
+    <div className="my-10 w-full">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-white">Hello World</h1>
-        <p className="text-2xl text-white">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+        <p className="text-2xl">
           Viewport size: {width}x{height}
         </p>
+        <UnderlineVagueSvg text="hello world comment ça va ?" />
       </div>
+      <CircleText text="hello ?" />
     </div>
   );
 }
---
+++ components/ui/specialWords/BgTextHover.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface BgTextHoverProps {}
+
+const BgTextHover: FC<BgTextHoverProps> = ({}) => {
+  return <div>BgTextHover</div>;
+};
+
+export default BgTextHover;
