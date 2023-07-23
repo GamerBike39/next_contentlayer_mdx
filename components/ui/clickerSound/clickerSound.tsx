9f94baa6b582438f8815a7b075c4206edd9672fd
GamerBike39 <champidub@gmail.com>
2023-07-23 15:16:22 +0200

    react confetti

--- components/ui/clickerSound/clickerSound.tsx
+++ components/ui/clickerSound/clickerSound.tsx
@@ -3,6 +3,8 @@ import { useEffect, useRef, useState } from "react";
 import style from "./clickerSound.module.css";
 import useSound from "use-sound";
 import { useSoundContext } from "@/providers/SoundProvider";
+import Confetti from "react-confetti";
+import { useViewportSize } from "@/hooks/use-viewport-size/use-viewport-size";
 
 interface LikeButtonProps {}
 
@@ -12,6 +14,8 @@ const LikeButton = ({}: LikeButtonProps) => {
   const refBouche = useRef<HTMLDivElement>(null);
   const refHeart = useRef<HTMLDivElement>(null);
   const [isPositive, setIsPositive] = useState<boolean>(true);
+  const { height, width } = useViewportSize();
+  const [showConfetti, setShowConfetti] = useState(false);
 
   // sound effect
   const soundUrl = "/sounds/glug-a.mp3";
@@ -39,6 +43,7 @@ const LikeButton = ({}: LikeButtonProps) => {
 
     if (isClicked > 100) {
       setIsClicked(100);
+      setShowConfetti(true); // Affiche le confetti lorsque isClicked atteint 100
       return isClicked;
     }
 
@@ -76,50 +81,72 @@ const LikeButton = ({}: LikeButtonProps) => {
     if (isClicked >= 100) {
       refHeart.current?.classList.add("scale-150");
       refHeart.current?.classList.add("-rotate-6");
+      setShowConfetti(true); // Affiche le confetti lorsque isClicked atteint 100
     } else {
       refHeart.current?.classList.remove("scale-150");
       refHeart.current?.classList.remove("-rotate-6");
     }
   }, [isClicked]);
 
+  useEffect(() => {
+    if (showConfetti) {
+      const timeout = setTimeout(() => {
+        setShowConfetti(false);
+      }, 5000); // 5000 ms = 5 secondes
+
+      // Assure-toi de nettoyer le timeout lorsque le composant est démonté
+      return () => clearTimeout(timeout);
+    }
+  }, [showConfetti]);
+
   return (
-    <div
-      ref={refHeart}
-      onClick={handleClick}
-      onContextMenu={rightClick}
-      className="relative w-max p-5 cursor-pointer hover:-rotate-6 hover:scale-150 transition-all ease-in-out duration-300 drop-shadow-xl"
-    >
-      <div className={`${style.heart}`}>
-        <div className={`${style.initialHeart}`} />
-        <div
-          className={`${style.heartAnimation}`}
-          style={{ height: `${isClicked}%` }}
-        />
-        <div className={`${style.eyeLeft}`} />
-        <div className={`${style.eyeRight}`} />
-        <div
-          ref={refBouche}
-          className={`absolute left-[15%] bottom-[25%] z-10 ${
-            isPositive ? " " : "transform rotate-180"
-          }}`}
-        >
-          <svg
-            width="16"
-            height="4"
-            viewBox="0 0 15 8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+    <>
+      <div
+        ref={refHeart}
+        onClick={handleClick}
+        onContextMenu={rightClick}
+        className="relative w-max p-5 cursor-pointer hover:-rotate-6 hover:scale-150 transition-all ease-in-out duration-300 drop-shadow-xl"
+      >
+        <div className={`${style.heart}`}>
+          <div className={`${style.initialHeart}`} />
+          <div
+            className={`${style.heartAnimation}`}
+            style={{ height: `${isClicked}%` }}
+          />
+          <div className={`${style.eyeLeft}`} />
+          <div className={`${style.eyeRight}`} />
+          <div
+            ref={refBouche}
+            className={`absolute left-[15%] bottom-[25%] z-10 ${
+              isPositive ? " " : "transform rotate-180"
+            }}`}
           >
-            <path
-              d="M2 2.44282C2.47317 5.16669 5.82087 6.22489 8.02885 5.96077C9.66761 5.76474 12.4542 3.71372 13 2"
-              stroke="black"
-              strokeWidth="3"
-              strokeLinecap="round"
-            />
-          </svg>
+            <svg
+              width="16"
+              height="4"
+              viewBox="0 0 15 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2 2.44282C2.47317 5.16669 5.82087 6.22489 8.02885 5.96077C9.66761 5.76474 12.4542 3.71372 13 2"
+                stroke="black"
+                strokeWidth="3"
+                strokeLinecap="round"
+              />
+            </svg>
+          </div>
         </div>
       </div>
-    </div>
+      {showConfetti && (
+        <Confetti
+          width={width - 20}
+          height={height}
+          gravity={0.5}
+          // numberOfPieces={100}
+        />
+      )}
+    </>
   );
 };
 
