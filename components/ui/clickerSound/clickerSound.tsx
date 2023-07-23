7ee2fc123fd9e322d7ec421388493c10e4d13fb0
GamerBike39 <champidub@gmail.com>
2023-07-23 02:43:14 +0200

    module.css for likeButton

--- components/ui/clickerSound/clickerSound.tsx
+++ components/ui/clickerSound/clickerSound.tsx
@@ -63,14 +63,32 @@ const LikeButton = ({}: LikeButtonProps) => {
     <div
       onClick={handleClick}
       onContextMenu={rightClick}
-      className="relative w-max p-5 cursor-pointer"
+      className="relative w-max p-5 cursor-pointer hover:-rotate-3 hover:scale-150 transition-all ease-in-out duration-300"
     >
       <div className={`${style.heart}  `}>
         <div className="bg-gray-400/60 w-8 h-[100%] absolute bottom-0 z-[2]" />
         <div
           className={`bg-red-400 w-8 absolute bottom-0 z-[3] transition-all ease-in`}
-          style={{ transition: "height 0.5s ease", height: `${isClicked}%` }}
+          style={{ height: `${isClicked}%` }}
         />
+        <div className={`${style.eyeLeft}`} />
+        <div className={`${style.eyeRight}`} />
+        <div className="absolute left-[15%] bottom-[25%] z-10">
+          <svg
+            width="16"
+            height="4"
+            viewBox="0 0 15 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 2.44282C2.47317 5.16669 5.82087 6.22489 8.02885 5.96077C9.66761 5.76474 12.4542 3.71372 13 2"
+              stroke="black"
+              strokeWidth="3"
+              strokeLinecap="round"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
