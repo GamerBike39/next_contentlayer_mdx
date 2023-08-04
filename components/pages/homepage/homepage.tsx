0a60836323730581eb0597be2e7040308d6b3ce9
GamerBike39 <champidub@gmail.com>
2023-08-04 18:02:29 +0200

    test perf on mobile

--- components/pages/homepage/homepage.tsx
+++ components/pages/homepage/homepage.tsx
@@ -11,6 +11,7 @@ import UnderlineVagueSvg from "@/components/ui/specialWords/Underline_Vague";
 import { useViewportSize } from "@/hooks/use-viewport-size/use-viewport-size";
 import { Luckiest_GuyFont } from "@/utils/fonts";
 import HomeHeader from "./header/HomeHeader";
+import Reveal from "@/components/animation/Reveal";
 
 interface HomepageProps {}
 
@@ -20,37 +21,47 @@ const Homepage = ({}) => {
   return (
     <>
       <HomeHeader />
-      <div className="my-10 w-full">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className={`text-8xl font-bold ${Luckiest_GuyFont.className} `}>
-            Hello World
-          </h1>
-          <p className="text-2xl">{/* Viewport size: {width}x{height} */}d</p>
-          <UnderlineVagueSvg text="hello world comment ça va ?" />
-        </div>
-        <AnimatedTextWord text="hello world sdqd" />
-        <AnimatedTextCharacter text="cqqcqc sc q scqscqs cqscq scq scqs cq scqscqscq sc q s q  " />
+      <div className="my-10 w-full max-w-4xl px-5 mx-auto">
+        <Reveal>
+          <div className="flex flex-col items-center justify-center">
+            <h1 className={`text-8xl font-bold ${Luckiest_GuyFont.className} `}>
+              Hello World
+            </h1>
+            <p className="text-2xl">{/* Viewport size: {width}x{height} */}d</p>
+            <UnderlineVagueSvg text="hello world comment ça va ?" />
+          </div>
+        </Reveal>
 
-        <p className="mt-5">
-          Comment ça va ? <RedLine text="hello world" /> t'en penses quoi ?
-        </p>
-        <div className="my-10">
-          <SVGHoverAnimation text="coucou" />
-        </div>
-        <div className="h-10 ">
-          <p>
-            comment ça va ? <BoxSpecialWord text="hello world" /> c'est cool ce
-            que tu fais, <BoxSpecialWord text="Gamerbike" /> , c'est vraiment
-            intéressant, <BoxSpecialWord text="Gamerbike" /> Mais dis moi c'est
-            quoi cette <UnderlineVagueSvg text="magie" />
+        <Reveal>
+          <p className="mt-5">
+            Comment ça va ? <RedLine text="hello world" /> t'en penses quoi ?
           </p>
-        </div>
-        <div className="my-20">
-          <LikeButton />
-        </div>
-        <CursorGradient>
-          <AccordionImg />
-        </CursorGradient>
+        </Reveal>
+        <Reveal>
+          <div className="my-10">
+            <SVGHoverAnimation text="coucou" />
+          </div>
+        </Reveal>
+        <Reveal>
+          <div className="h-10 ">
+            <p>
+              comment ça va ? <BoxSpecialWord text="hello world" /> c'est cool
+              ce que tu fais, <BoxSpecialWord text="Gamerbike" /> , c'est
+              vraiment intéressant, <BoxSpecialWord text="Gamerbike" /> Mais dis
+              moi c'est quoi cette <UnderlineVagueSvg text="magie" />
+            </p>
+          </div>
+        </Reveal>
+        <Reveal>
+          <div className="my-20">
+            <LikeButton />
+          </div>
+        </Reveal>
+        <Reveal>
+          <CursorGradient>
+            <AccordionImg />
+          </CursorGradient>
+        </Reveal>
       </div>
     </>
   );
